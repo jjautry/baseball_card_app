3adc914d88e7f638e5fae86b326183f8db838c7b
jjautry <52764049+jjautry@users.noreply.github.com>
2022-03-07 21:46:07 -0500

    basic structure for webapp
    - added bootstrap css and js
    - created starter templates
    - created template dropdowns for homepage to search card info

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from user import add_new_card, search_dict4card
 
 app = Flask(__name__)
 
-@app.route('/', methods='POST')
+@app.route('/')
 def index():
-    return render_template()
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
